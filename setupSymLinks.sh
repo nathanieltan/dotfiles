92cbb2f4297415240b0e4d283a443d16b5e6ac2d
Nathaniel Tan <nathaniel98.tan@gmail.com>
2022-08-04 19:15:02 +0000

    fixed symlink script

--- setupSymLinks.sh
+++ setupSymLinks.sh
@@ -1,4 +1,9 @@
-ln -s nvim ~/.config/nvim
-ln -s kitty ~/.config/kitty
-ln -s alacritty ~/.config/alacritty
-ln -s .zshrc ~/.zshrc
+rm -R ~/.config/nvim
+rm -R ~/.config/kitty
+rm -R ~/.config/alacritty
+rm -R ~/.zshrc
+
+ln -s "$PWD/nvim" "/home/$USER/.config/nvim"
+ln -s "$PWD/kitty" "/home/$USER/.config/kitty"
+ln -s "$PWD/alacritty" "/home/$USER/.config/alacritty"
+ln -s "$PWD/.zshrc" "/home/$USER/.zshrc"
